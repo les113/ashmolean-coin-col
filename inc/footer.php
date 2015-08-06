ed985a36f2d47525bc4f8fb30177c91ad7ca618f
les <les.lumb@it.ox.ac.uk>
2015-08-06 10:02:32 +0100

    footer completed

--- inc/footer.php
+++ inc/footer.php
@@ -2,16 +2,6 @@
         <div class="container">  
             
             <div id="prefooter">
-				<ul id="social-links">
-					<a href=""><li class="fa fa-2x fa-twitter"><h2>Twitter</h2></li></a>
-					<a href=""><li class="fa fa-2x fa-linkedin"><h2>LinkedIn</h2></li>
-					<a href=""><li class="fa fa-2x fa-facebook"><h2>Facebook</h2></li>
-					<a href=""><li class="fa fa-2x fa-pinterest"><h2>Pinterest</h2></li>
-					<a href=""><li class="fa fa-2x fa-youtube"><h2>Youtube</h2></li>
-					<a href=""><li class="fa fa-2x fa-skype"><h2>Skype</h2></li>
-					<a href=""><li class="fa fa-2x fa-google-plus"><h2>Google Plus</h2></li>
-					<a href=""><li class="fa fa-2x fa-tumblr"><h2>Tumblr</h2></li>
-				</ul>
             </div>
             
  		</div> <!-- end container -->
@@ -23,21 +13,33 @@
 
 			<footer id="footer">
 				<div class="row">
-					  <div class="col-md-8 col-lg-10 sponsors">
+					  <div class="col-md-3 sponsors">
 						<a href="http://www.ox.ac.uk" target="_blank"><img src="img/ox-logo.gif" alt="Oxford University"/></a>
-						<a href="http://www.romaneconomy.ox.ac.uk" target="_blank"><img src="img/romaneconomypj-logo.gif" alt="The Oxford Roman Economy Project"/></a>
-						<a href="http://www.ashmolean.org" target="_blank"><img src="img/ashmolean-logo.png" alt="The Ashmolean Museum"/></a>
-                        <a href="#" target="_blank"><img src="img/augustusfoundation-logo.png" alt="The Augustus Foundation"/></a>
+                <ul id="social-links">
+					<a href=""><li class="fa fa-twitter"><h2>Twitter</h2></li></a>
+					<a href=""><li class="fa fa-linkedin"><h2>LinkedIn</h2></li></a>
+					<a href=""><li class="fa fa-facebook"><h2>Facebook</h2></li></a>
+					<a href=""><li class="fa fa-pinterest"><h2>Pinterest</h2></li></a>
+					<a href=""><li class="fa fa-youtube"><h2>Youtube</h2></li></a>
+					<a href=""><li class="fa fa-skype"><h2>Skype</h2></li></a>
+					<a href=""><li class="fa fa-google-plus"><h2>Google Plus</h2></li></a>
+					<a href=""><li class="fa fa-tumblr"><h2>Tumblr</h2></li></a>
+				</ul>
 					  </div>                      
                       
-					  <div class="col-md-4 col-lg-2 footer-links">
-						<h4>Sub Menu</h4>
-						<ul class="">
-							<li><a href="#">Link</li></a>
-							<li><a href="#">Link</li></a>
-							<li><a href="#">Link</li></a>
-							<li><a href="#">Link</li></a>
-						</ul>
+					  <div class="col-md-2 footer-contacts">
+						<h4>Contact Us</h4>
+							<p>Heberden Coin Room<br/>
+							Ashmolean Museum<br/>
+							Beaumont Street<br/>
+							Oxford<br/>
+                            OX1 2PH</p>
+					  </div>
+					  <div class="col-md-3 footer-contacts">
+						<h4>&nbsp;</h4>
+							<p>T: +44 (0)1865 278058<br/>
+							F: +44 (0)1865 278057<br/>
+							E: coin-room@ashmus.ox.ac.uk</p>
 					  </div>
 				</div>
                 
